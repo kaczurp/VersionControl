37016cf428632e9a68af365c051de4cc8fb83dd2
Péter Kaczur <peter.kaczur@stud.uni-corvinus.hu>
2019-11-26 17:16:57 +0100

    wrcommit

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.txtFullName = new System.Windows.Forms.TextBox();
             this.lblFullName = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.wrtButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -68,11 +69,22 @@
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
+            // wrtButton
+            // 
+            this.wrtButton.Location = new System.Drawing.Point(296, 94);
+            this.wrtButton.Name = "wrtButton";
+            this.wrtButton.Size = new System.Drawing.Size(151, 23);
+            this.wrtButton.TabIndex = 6;
+            this.wrtButton.Text = "button1";
+            this.wrtButton.UseVisualStyleBackColor = true;
+            this.wrtButton.Click += new System.EventHandler(this.wrtButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wrtButton);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lblFullName);
             this.Controls.Add(this.txtFullName);
@@ -90,6 +102,7 @@
         private System.Windows.Forms.TextBox txtFullName;
         private System.Windows.Forms.Label lblFullName;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button wrtButton;
     }
 }
 
